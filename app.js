43d03943f586cac1cf0cdf492a12f084cc076267
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-07-02 20:53:31 +0700

    upload source code

--- app.js
+++ app.js
@@ -1,45 +1,23 @@
-const day = document.querySelector('.countdown_day');
-const hour = document.querySelector('.countdown_hour');
-const minute = document.querySelector('.countdown_minutes');
-const second = document.querySelector('.countdown_second');
-
-//Sat Jul 02 2022 15:17:09 GMT+0700 (Giờ Đông Dương)
-
-//Sat Jul 10 2022 15:17:09 GMT+0700 (Giờ Đông Dương)
-
+const dayText = document.querySelector('.countdown_day');
+const hourText = document.querySelector('.countdown_hour');
+const minuteText = document.querySelector('.countdown_minutes');
+const secondText = document.querySelector('.countdown_second');
 
 function countDown(date){
     let now = new Date();
     let startDay = now.getTime();
     let endDay = (new Date(date)).getTime();
-
     let timeRemaining = parseInt((endDay - startDay) / 1000);
+    let day = parseInt(timeRemaining / 86400)
+    let hour = parseInt(timeRemaining % 86400 / 3600)
+    let minute = parseInt(timeRemaining % 86400 % 3600 / 60)
+    let second = timeRemaining % 86400 % 3600 % 60
 
-    // 1 day = 86400 second
-    // 1 hour = 3600 second
-
-    if(timeRemaining >= 86400){
-        day.textContent = parseInt(timeRemaining / 86400).toString()
-    }
-    let resFloatDay = timeRemaining / 86400
-    if((timeRemaining - 86400 * parseInt(resFloatDay))  >= 3600){
-        hour.textContent = parseInt((timeRemaining - 86400 * parseInt(resFloatDay)) / 3600).toString() 
-    }
-    let resFloatHour = ((timeRemaining - 86400 * parseInt(resFloatDay))) / 3600;
-
-    if(resFloatHour * 3600 - parseInt(resFloatHour) * 3600 >= 60){
-        minute.textContent = parseInt((resFloatHour * 3600 - parseInt(resFloatHour) * 3600) / 60)
-    }
-    let resFloatMinute = (resFloatHour * 3600 - parseInt(resFloatHour) * 3600) / 60
-
-
-    if(resFloatMinute * 60 - (parseInt(resFloatMinute) * 60) < 60){
-        second.textContent = (resFloatMinute * 60 - (parseInt(resFloatMinute) * 60)).toString()
-    }
+    dayText.textContent = `0${day}`.slice(-2)
+    hourText.textContent = `0${hour}`.slice(-2)
+    minuteText.textContent = `0${minute}`.slice(-2)
+    secondText.textContent = `0${second}`.slice(-2)
 }
 setInterval(function(){
     countDown('Sep 16 2022 00:00:00 GMT+0700 (Giờ Đông Dương)')
-}, 1)
-
-
-
+}, 1000)
