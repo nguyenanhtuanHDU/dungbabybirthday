6efafacef817913bf66a76adf525500f70054a17
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-07-14 16:51:39 +0700

    upload source code

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ const minuteText = document.querySelector('.countdown_minutes');
 const secondText = document.querySelector('.countdown_second');
 const countdownSlider = document.querySelector('.countdown_slider');
 const coudown_Item = document.querySelectorAll('.coudown__item');
+const container = document.querySelector('.container')
+
+const linearColor = [
+    '#ec008c',
+    '#fc6767',
+    '#1488CC',
+    '#2B32B2',
+    '#00467F',
+    '#A5CC82', 
+    '#076585',
+    '#fff',
+    '#9796f0',
+    '#fbc7d4',
+    '#acb6e5',
+    '#86fde8',
+    '#FFE000',
+    '#799F0C',
+    '#00416A',
+    '#ffe259',
+    '#ffa751'
+    ,'#799F0C',
+    '#ACBB78',
+    '#5433FF',
+    '#A5FECB',
+    '#20BDFF',
+    '#ee9ca7',
+    '#ffdde1',
+    '#6dd5ed',
+    '#2193b0'
+]
+const heartTemplate = `      
+<div class="heart">
+    <i class="fa-solid fa-heart"></i>
+</div>`
 
 function countDown(date){
     let now = new Date();
@@ -30,3 +64,17 @@ setInterval(function(){
     if(spaceScroll === coudown_Item[0].offsetWidth * coudown_Item.length) spaceScroll = 0
     countdownSlider.scrollLeft = spaceScroll
 }, 3000)
+
+let index;
+
+document.addEventListener('click', function(e){
+    index = Math.floor(Math.random() * linearColor.length)
+    document.body.insertAdjacentHTML('afterbegin', heartTemplate)
+    const heart = document.querySelector('.heart')
+    heart.style.color = `${linearColor[index]}`
+    heart.style.left = `${e.pageX}px`
+    heart.style.top = `${e.pageY}px`
+    setTimeout(function(){
+        document.body.removeChild(heart)
+    }, 2000)
+})
